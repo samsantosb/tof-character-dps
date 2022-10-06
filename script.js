5ef12c66379ce252a45712e3b7c1fa704efc4e8b
samsantosb <samsantosb@outlook.com>
2022-10-06 18:19:34 -0300

    updated character

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //find character_critical and character_damage id, sum them and display in character_total_damage id
 
-
-/*validators*/
-
-
 function calculateDamage() {
-    const critical = document.getElementById("character_critical").value;
-    const damage = document.getElementById("character_attack").value;
-    const totalDamage = parseInt(critical) + parseInt(damage);
-    console.log(totalDamage)
+    const critical = Number(document.getElementById("character_critical").value);
+    const criticalPercent = critical * 0.006276
+
+
+    const criticalDamage = 0.5;
+    const criticalDamageBuff = criticalDamage * (criticalPercent / 100) + 1;
+
+    const damage = Number(document.getElementById("character_elemental_attack").value);
+
+    const totalDamage = damage * criticalDamageBuff;
+
+    const totalDamageRounded = totalDamage.toFixed(2);
+    console.log(totalDamageRounded, 'dano total do personagem');
+
     document.getElementById("character_total").value = totalDamage;
 }
 
@@ -16,26 +22,36 @@ function calculateDamage() {
 //find glove_critical and glove_damage id, sum them and display in glove_total_damage id
 function calculateGloveDamage() {
     const critical = document.getElementById("glove_critical").value;
-    const damage = document.getElementById("glove_attack").value;
-    const totalDamage = parseInt(critical) + parseInt(damage);
+
+    const elementalAttack = document.getElementById("glove_elemental_attack").value;
+
+    const attack = document.getElementById("glove_attack").value;
+
+    const totalDamage = Number(critical) + Number(attack) + Number(elementalAttack);
+    console.log(totalDamage, 'dano total do luva');
+
     document.getElementById("glove_total").value = totalDamage;
-    console.log(totalDamage);
 }
 
 //find weapon_critical and weapon_damage id, sum them and display in weapon_total_damage id
-function calculateWeaponDamage() {
+function calculateBootDamage() {
     const critical = document.getElementById("boot_critical").value;
-    const damage = document.getElementById("boot_attack").value;
-    const totalDamage = parseInt(critical) + parseInt(damage);
+
+    const elementalAttack = document.getElementById("boot_elemental_attack").value;
+
+    const attack = document.getElementById("boot_attack").value;
+
+    const totalDamage = Number(critical) + Number(attack) + Number(elementalAttack);
+    console.log(totalDamage, 'dano total da bota');
+
     document.getElementById("boot_total").value = totalDamage;
-    console.log(totalDamage)
 }
 
 //create a function that trigger the functions when submit is clicked
 function submit() {
     calculateDamage();
     calculateGloveDamage();
-    calculateWeaponDamage();
+    calculateBootDamage();
 }
 
 //create a function that trigger subbmit function when button is clicked
